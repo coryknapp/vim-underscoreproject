d7ff45d9b1a5a31572e39e6e104241bc78d64529
Cory Knapp <knapp.cory@gmail.com>
2016-06-13 17:08:15 -0700

    fixed bug python bug

--- plugin/underscoreproject.py
+++ plugin/underscoreproject.py
@@ -6,7 +6,8 @@ from sys import argv
 import os
 
 path = argv[1]
-while path:
+# test to make sure where not at the root
+while os.path.dirname(path) != path:
     path, filename = os.path.split(path)
     underscore_proj_path = os.path.join( path, "_proj.vim" )
     if os.path.exists(underscore_proj_path):
